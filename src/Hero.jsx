cb7c32596367ccf2ac069638bb4a3c6adf60b1f0
Aditya Gupta <87805459+impulseadi@users.noreply.github.com>
2025-03-28 01:12:59 +0530

    Update Hero.jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,13 +3,15 @@ import React from "react";
 const Hero = () => {
   return (
     <div
-      className="h-screen w-full bg-cover bg-center flex items-center justify-center"
-      style={{ backgroundImage: "url('https://images.hdqwalls.com/download/the-batman-2020-logo-4k-ek-1920x1080.jpg')" }}
+      className="h-screen w-full bg-cover bg-center flex items-end justify-center pb-16 md:pb-24 lg:pb-32"
+      style={{
+        backgroundImage:
+          "url('https://images.hdqwalls.com/download/the-batman-2020-logo-4k-ek-1920x1080.jpg')",
+      }}
     >
-     <h1 className="text-6xl mt-105 font-bold bg-gradient-to-r from-[#f41e14] via-red-600 to-[#f41e14] text-transparent bg-clip-text">
-  Welcome to the Batcave
-</h1>
-
+      <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold bg-gradient-to-r from-[#f41e14] via-red-600 to-[#f41e14] text-transparent bg-clip-text">
+        Welcome to the Batcave
+      </h1>
     </div>
   );
 };
